6b514bb02a7193539e59c8005fc76a8c0925c58b
abdulla-rez <abdulla.s@reizend.ai>
2025-06-24 17:37:15 +0530

    COmmit

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -5,15 +5,29 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// export const AppDataSource = new DataSource({
+//   type: 'postgres',
+//   host: process.env.DB_HOST,
+//   port: parseInt(process.env.DB_PORT || '5432'),
+//   username: process.env.DB_USERNAME,
+//   url:process.env.DATABASE_URL,
+//   password: process.env.DB_PASSWORD,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+//   database: process.env.DB_NAME,
+//   synchronize: false,
+//   logging: false,
+//   entities: [User, ResetToken],
+// });
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME,
-  url:process.env.DATABASE_URL,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  synchronize: true,
+  url: process.env.DATABASE_URL,         // ✅ Use only url
+  ssl: {
+    rejectUnauthorized: false,           // ✅ Render requires SSL
+  },
+  synchronize: false,
   logging: false,
   entities: [User, ResetToken],
 });
